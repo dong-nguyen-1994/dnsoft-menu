37010fad127a2561f2f1e4984722ae6ba9b85249
Dong Nguyen <dongbuh@gmail.com>
2021-04-08 15:19:52 +0700

    Add language

---
+++ resources/lang/vi/menu-item.php
@@ -0,0 +1,41 @@
+<?php
+return [
+    'label'              => 'Label',
+    'class'              => 'CSS Class',
+    'self'               => 'Same tab',
+    'blank'              => 'New Tab',
+    'icon'               => 'Icon',
+    'icon_helper'        => 'Ex: <code>&#x3C;i class=&#x22;fa fa-home&#x22;&#x3E;&#x3C;/i&#x3E;</code>',
+    'target'             => 'Target',
+    'url'                => 'URL',
+    'menu_id'            => 'Menu ID',
+    'menu_builder_class' => 'Builder Type',
+    'parent'             => 'Parent',
+    'image'             => 'Image',
+
+    'index' => [
+        'page_title'    => 'Menu Item',
+        'page_subtitle' => 'Menu Item',
+        'breadcrumb'    => 'Menu Item',
+    ],
+
+    'create' => [
+        'page_title'    => 'Add Menu Item',
+        'page_subtitle' => 'Add Menu Item',
+        'breadcrumb'    => 'Add Item',
+    ],
+
+    'edit' => [
+        'page_title'    => 'Edit Menu Item',
+        'page_subtitle' => 'Edit Menu Item',
+        'breadcrumb'    => 'Edit',
+    ],
+
+    'notification' => [
+        'created' => 'Menu Item successfully created!',
+        'updated' => 'Menu Item successfully updated!',
+        'deleted' => 'Menu Item successfully deleted!',
+        'move_up' => 'Menu Item successfully move up!',
+        'move_down' => 'Menu Item successfully move down!',
+    ],
+];
